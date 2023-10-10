f24fc9ce84fb54c8d5e4269761071d9cbc567f30
Danil Prisiazhnyi <judas.priest999@gmail.com>
2023-10-10 05:33:22 +0300

    fixed Http factory

--- pip-services4-http-go/build/DefaultHttpFactory.go
+++ pip-services4-http-go/build/DefaultHttpFactory.go
@@ -6,19 +6,19 @@ import (
 	services "github.com/pip-services4/pip-services4-go/pip-services4-http-go/controllers"
 )
 
-// DefaultRpcFactory are creates RPC components by their descriptors
+// DefaultHttpFactory are creates RPC components by their descriptors
 //
 //	see Factory
 //	see HttpEndpoint
 //	see HeartbeatRestController
 //	see StatusRestController
-type DefaultRpcFactory struct {
+type DefaultHttpFactory struct {
 	cbuild.Factory
 }
 
-// NewDefaultRpcFactory creates a new instance of the factory.
-func NewDefaultRpcFactory() *DefaultRpcFactory {
-	c := DefaultRpcFactory{}
+// NewDefaultHttpFactory creates a new instance of the factory.
+func NewDefaultHttpFactory() *DefaultHttpFactory {
+	c := DefaultHttpFactory{}
 	c.Factory = *cbuild.NewFactory()
 
 	httpEndpointDescriptor := cref.NewDescriptor("pip-services", "endpoint", "http", "*", "1.0")
